febf4c6784f8b54c6bfe22c4f434bdb37f7e6fe8
Jingun Hong <jingun.hong@gmail.com>
2021-10-02 12:42:30 +0900

    greet every 2 seconds

--- src/main.rs
+++ src/main.rs
@@ -8,20 +8,29 @@ fn add_people(mut commands: Commands) {
     commands.spawn().insert(Person).insert(Name("Bob".to_string()));
 }
 
-fn hello_world() {
-    println!("hello world!")
+struct GreetTimer(Timer);
+
+fn greet_people(time:Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("hello {}!", name.0)
+        }
+    }
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("hello {}!", name.0)
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut AppBuilder) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people.system())
+            .add_system(greet_people.system());
     }
 }
 
 fn main() {
     App::build()
-        .add_startup_system(add_people.system())
-        .add_system(hello_world.system())
-        .add_system(greet_people.system())
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
         .run();
 }
